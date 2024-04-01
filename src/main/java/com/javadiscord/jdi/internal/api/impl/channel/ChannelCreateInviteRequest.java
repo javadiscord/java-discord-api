e6c485ee75e54b7f8b25e4ee2b4eefd3cba6787e
Suraj Kumar <sk96.uk@gmail.com>
2024-04-01 13:54:12 +0100

    Fixed ChannelCreateInviteRequest to use Map.of

--- src/main/java/com/javadiscord/jdi/internal/api/impl/channel/ChannelCreateInviteRequest.java
+++ src/main/java/com/javadiscord/jdi/internal/api/impl/channel/ChannelCreateInviteRequest.java
@@ -3,6 +3,8 @@ package com.javadiscord.jdi.internal.api.impl.channel;
 import com.javadiscord.jdi.internal.api.DiscordRequest;
 import com.javadiscord.jdi.internal.api.DiscordRequestBuilder;
 
+import java.util.Map;
+
 public record ChannelCreateInviteRequest(long channelId, int maxAge, int maxUses, boolean temporary)
         implements DiscordRequest {
 
@@ -11,6 +13,10 @@ public record ChannelCreateInviteRequest(long channelId, int maxAge, int maxUses
         return new DiscordRequestBuilder()
                 .post()
                 .url("/channels/%s/invites".formatted(channelId))
-                .body(this);
+                .body(
+                        Map.of(
+                                "max_age", maxAge,
+                                "max_uses", maxUses,
+                                "temporary", temporary));
     }
 }
