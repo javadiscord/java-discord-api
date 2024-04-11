a2dfba3a676c93108835ebf1fe4d635ee648a618
Tanish Azad <tanishazad03@gmail.com>
2024-04-12 05:28:33 +0530

    fixed imports

--- src/main/java/com/javadiscord/jdi/internal/gateway/handlers/events/codec/decoders/ThreadListSyncDecoder.java
+++ src/main/java/com/javadiscord/jdi/internal/gateway/handlers/events/codec/decoders/ThreadListSyncDecoder.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.javadiscord.jdi.internal.gateway.GatewayEvent;
 import com.javadiscord.jdi.internal.gateway.handlers.events.codec.EventDecoder;
-import com.javadiscord.jdi.internal.models.channel.ThreadSync;
+import com.javadiscord.jdi.internal.gateway.handlers.events.codec.models.channel.ThreadSync;
 
 public class ThreadListSyncDecoder implements EventDecoder<ThreadSync> {
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
