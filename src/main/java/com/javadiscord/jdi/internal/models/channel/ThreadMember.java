ec707c6c693748780b40e2259669f3660d3cfa30
Tanish Azad <tanishazad03@gmail.com>
2024-04-12 00:40:43 +0530

    spotlessApply

--- src/main/java/com/javadiscord/jdi/internal/models/channel/ThreadMember.java
+++ src/main/java/com/javadiscord/jdi/internal/models/channel/ThreadMember.java
@@ -1,17 +1,18 @@
 package com.javadiscord.jdi.internal.models.channel;
 
-import java.time.OffsetDateTime;
-
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.javadiscord.jdi.internal.models.guild.Member;
+
+import java.time.OffsetDateTime;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record ThreadMember(
         @JsonProperty("id") long threadId,
         @JsonProperty("user_id") long userId,
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
-        @JsonProperty("join_timestamp") OffsetDateTime joinTime,
+                @JsonProperty("join_timestamp")
+                OffsetDateTime joinTime,
         @JsonProperty("flags") int flags,
         @JsonProperty("member") Member member) {}
