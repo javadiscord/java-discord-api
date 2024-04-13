960a9fecb1c276896c8c1a9baf3c6686158de1ee
Tanish Azad <tanishazad03@gmail.com>
2024-04-13 07:49:20 +0530

    added  missing annotation

--- src/main/java/com/javadiscord/jdi/internal/models/guild/IntegrationAccount.java
+++ src/main/java/com/javadiscord/jdi/internal/models/guild/IntegrationAccount.java
@@ -1,8 +1,8 @@
 package com.javadiscord.jdi.internal.models.guild;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record IntegrationAccount(
-    @JsonProperty("id") String id,
-    @JsonProperty("name") String name
-) { }
+        @JsonProperty("id") String id, @JsonProperty("name") String name) {}
