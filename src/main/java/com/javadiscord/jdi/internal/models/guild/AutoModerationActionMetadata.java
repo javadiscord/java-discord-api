3a9f471562a2f4816dea4027464446bf5193ebdc
Nathan Weisz <151496498+nateweisz@users.noreply.github.com>
2024-04-17 05:06:23 -0700

    Auto Moderation API (#52)
    Auto Moderation API

--- src/main/java/com/javadiscord/jdi/internal/models/guild/AutoModerationActionMetadata.java
+++ src/main/java/com/javadiscord/jdi/internal/models/guild/AutoModerationActionMetadata.java
@@ -3,6 +3,7 @@ package com.javadiscord.jdi.internal.models.guild;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+// https://discord.com/developers/docs/resources/auto-moderation#auto-moderation-action-object-action-metadata read this to see when fields will be present
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record AutoModerationActionMetadata(
         @JsonProperty("channel_id") long channelId,
