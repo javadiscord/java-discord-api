a24fa035e560efcccb6629b61e13c05a46e0cb4f
Tasnim Ferdous <tferdous611@gmail.com>
2024-04-17 22:53:53 -0400

    applied spotless

--- src/main/java/com/javadiscord/jdi/internal/api/impl/channel/GetChannelInvitesRequest.java
+++ src/main/java/com/javadiscord/jdi/internal/api/impl/channel/GetChannelInvitesRequest.java
@@ -1,6 +1,5 @@
 package com.javadiscord.jdi.internal.api.impl.channel;
 
-
 import com.javadiscord.jdi.internal.api.DiscordRequest;
 import com.javadiscord.jdi.internal.api.DiscordRequestBuilder;
 
@@ -8,8 +7,6 @@ public record GetChannelInvitesRequest(long channelId) implements DiscordRequest
 
     @Override
     public DiscordRequestBuilder create() {
-        return new DiscordRequestBuilder()
-                .get()
-                .path("/channels/%s/invites".formatted(channelId));
+        return new DiscordRequestBuilder().get().path("/channels/%s/invites".formatted(channelId));
     }
 }
--- src/main/java/com/javadiscord/jdi/internal/api/impl/channel/GroupDMAddRecipientRequest.java
+++ src/main/java/com/javadiscord/jdi/internal/api/impl/channel/GroupDMAddRecipientRequest.java
@@ -4,10 +4,7 @@ import com.javadiscord.jdi.internal.api.DiscordRequest;
 import com.javadiscord.jdi.internal.api.DiscordRequestBuilder;
 
 public record GroupDMAddRecipientRequest(
-        long channelId,
-        long userId,
-        String accessToken,
-        String nickname)
+        long channelId, long userId, String accessToken, String nickname)
         implements DiscordRequest {
 
     @Override
