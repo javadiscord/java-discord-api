80a90c596cb01b5da12d002f924f760d522dcfc5
Tasnim Ferdous <109835247+tferdous17@users.noreply.github.com>
2024-04-17 23:44:53 -0400

    additional channel endpoints (#60)

---
+++ src/main/java/com/javadiscord/jdi/internal/api/impl/channel/GroupDMAddRecipientRequest.java
@@ -0,0 +1,16 @@
+package com.javadiscord.jdi.internal.api.impl.channel;
+
+import com.javadiscord.jdi.internal.api.DiscordRequest;
+import com.javadiscord.jdi.internal.api.DiscordRequestBuilder;
+
+public record GroupDMAddRecipientRequest(
+        long channelId, long userId, String accessToken, String nickname)
+        implements DiscordRequest {
+
+    @Override
+    public DiscordRequestBuilder create() {
+        return new DiscordRequestBuilder()
+                .put()
+                .path("/channels/%s/recipients/%s".formatted(channelId, userId));
+    }
+}
