259b1c8e795850b3c3ad628e9b6c5c150effcb4c
Nathan Weisz <nathan@weisz.org>
2024-04-19 14:00:10 -0700

    remove audit log changes

--- src/main/java/com/javadiscord/jdi/internal/models/auditlog/AuditLogEntry.java
+++ src/main/java/com/javadiscord/jdi/internal/models/auditlog/AuditLogEntry.java
@@ -5,10 +5,10 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+// missing auditLogChanges
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record AuditLogEntry(
     @JsonProperty("target_id") String targetId,
-    @JsonProperty("changes") List<AuditLogChange> auditLogChanges,
     @JsonProperty("user_id") long userId,
     @JsonProperty("id") long id,
     @JsonProperty("action_type") AuditLogEvent actionType,
