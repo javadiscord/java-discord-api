f7d7f41df4a2a4514bc9334a0cc061cd5741c76c
Tasnim Ferdous <tferdous611@gmail.com>
2024-04-19 17:26:07 -0400

    refactor: include overwriteId

--- src/main/java/com/javadiscord/jdi/internal/api/impl/channel/DeleteChannelRequest.java
+++ src/main/java/com/javadiscord/jdi/internal/api/impl/channel/DeleteChannelRequest.java
@@ -3,10 +3,12 @@ package com.javadiscord.jdi.internal.api.impl.channel;
 import com.javadiscord.jdi.internal.api.DiscordRequest;
 import com.javadiscord.jdi.internal.api.DiscordRequestBuilder;
 
-public record DeleteChannelRequest(long channelId) implements DiscordRequest {
+public record DeleteChannelRequest(long channelId, long overwriteId) implements DiscordRequest {
 
     @Override
     public DiscordRequestBuilder create() {
-        return new DiscordRequestBuilder().delete().path("/channels/%s".formatted(channelId));
+        return new DiscordRequestBuilder()
+                .delete()
+                .path("/channels/%s/permissions/%s".formatted(channelId, overwriteId));
     }
 }
