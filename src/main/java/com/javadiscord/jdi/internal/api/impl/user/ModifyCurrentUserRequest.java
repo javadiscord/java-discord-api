38ec673edf2f81b8c3b7b43d3e4f13dd335c21f2
Tanish Azad <tanishazad03@gmail.com>
2024-04-21 04:01:16 +0530

    fixed modify user request

--- src/main/java/com/javadiscord/jdi/internal/api/impl/user/ModifyCurrentUserRequest.java
+++ src/main/java/com/javadiscord/jdi/internal/api/impl/user/ModifyCurrentUserRequest.java
@@ -1,26 +1,15 @@
 package com.javadiscord.jdi.internal.api.impl.user;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.Base64;
 import java.util.Map;
 
 import com.javadiscord.jdi.internal.api.DiscordRequest;
 import com.javadiscord.jdi.internal.api.DiscordRequestBuilder;
 
-public final record ModifyCurrentUserRequest(String username, Path avatarPath) implements DiscordRequest {
+public final record ModifyCurrentUserRequest(String username, byte[] avatarImage, String type) implements DiscordRequest {
     @Override
     public DiscordRequestBuilder create() {
-        byte[] avatarContent = new byte[0];
-
-        try {
-            avatarContent = Files.readAllBytes(avatarPath);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        String encodedAvatar = "data:image/jpeg;base64," + Base64.getEncoder().encodeToString(avatarContent);
+        String encodedAvatar = "data:image/%s;base64,%s".formatted(type, Base64.getEncoder().encodeToString(avatarImage));
 
         return new DiscordRequestBuilder()
             .path("/users/@me")
