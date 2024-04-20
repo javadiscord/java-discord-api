be824d2ec5e8b7dc04728280e4e49ef27db6f262
Tasnim Ferdous <tferdous611@gmail.com>
2024-04-20 19:40:14 -0400

    implement application endpoints

---
+++ src/main/java/com/javadiscord/jdi/internal/api/impl/application/GetCurrentApplicationRequest.java
@@ -0,0 +1,12 @@
+package com.javadiscord.jdi.internal.api.impl.application;
+
+import com.javadiscord.jdi.internal.api.DiscordRequest;
+import com.javadiscord.jdi.internal.api.DiscordRequestBuilder;
+
+public record GetCurrentApplicationRequest() implements DiscordRequest {
+
+    @Override
+    public DiscordRequestBuilder create() {
+        return new DiscordRequestBuilder().get().path("/applications/@me");
+    }
+}
