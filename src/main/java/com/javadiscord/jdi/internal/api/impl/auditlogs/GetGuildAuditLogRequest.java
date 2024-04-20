d3305ad23e4c3f94024eff066880a147c4730907
Tasnim Ferdous <tferdous611@gmail.com>
2024-04-20 19:17:34 -0400

    rebased and applied spotless

--- src/main/java/com/javadiscord/jdi/internal/api/impl/auditlogs/GetGuildAuditLogRequest.java
+++ src/main/java/com/javadiscord/jdi/internal/api/impl/auditlogs/GetGuildAuditLogRequest.java
@@ -1,23 +1,24 @@
 package com.javadiscord.jdi.internal.api.impl.auditlogs;
 
+import java.util.Optional;
+
 import com.javadiscord.jdi.internal.api.DiscordRequest;
 import com.javadiscord.jdi.internal.api.DiscordRequestBuilder;
 
-import java.util.Optional;
-
 public record GetGuildAuditLogRequest(
-        long guildId,
-        Optional<Long> userId,
-        Optional<Integer> actionType,
-        Optional<Long> before,
-        Optional<Long> after,
-        Optional<Integer> limit)
-        implements DiscordRequest {
+    long guildId,
+    Optional<Long> userId,
+    Optional<Integer> actionType,
+    Optional<Long> before,
+    Optional<Long> after,
+    Optional<Integer> limit
+)
+    implements DiscordRequest {
 
     @Override
     public DiscordRequestBuilder create() {
-        DiscordRequestBuilder discordRequestBuilder =
-                new DiscordRequestBuilder().get().path("/guilds/%s/audit-logs".formatted(guildId));
+        DiscordRequestBuilder discordRequestBuilder = new DiscordRequestBuilder().get()
+            .path("/guilds/%s/audit-logs".formatted(guildId));
 
         userId.ifPresent(val -> discordRequestBuilder.queryParam("user_id", val));
         actionType.ifPresent(val -> discordRequestBuilder.queryParam("action_type", val));
