b17865cd7a66138f35af8ccbecc0188eb24ad6f7
Nathan Weisz <nathan@weisz.org>
2024-04-22 13:14:08 -0700

    final touched: enum pr

--- src/main/java/com/javadiscord/jdi/internal/models/guild/WidgetObject.java
+++ src/main/java/com/javadiscord/jdi/internal/models/guild/WidgetObject.java
@@ -2,14 +2,17 @@ package com.javadiscord.jdi.internal.models.guild;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.javadiscord.jdi.internal.models.channel.Channel;
+import com.javadiscord.jdi.internal.models.user.User;
+
+import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record WidgetObject(
-    @JsonProperty("id") long id,
-    @JsonProperty("name") String name,
-    @JsonProperty("instant_invite") String instantInvite,
-    // @JsonProperty("channels") List<PartialChannel> channels, // TODO: future
-    // developer implement these both
-    // @JsonProperty("members") List<PartialUser> members,
-    @JsonProperty("presence_count") int presenceCount
+        @JsonProperty("id") long id,
+        @JsonProperty("name") String name,
+        @JsonProperty("instant_invite") String instantInvite,
+        @JsonProperty("channels") List<Channel> channels,
+        @JsonProperty("members") List<User> members,
+        @JsonProperty("presence_count") int presenceCount
 ) {}
