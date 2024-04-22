6f5f2d7697a294c0a319204c4ec054c2fbbea159
Tanish Azad <tanishazad03@gmail.com>
2024-04-22 08:29:05 +0530

    fixed requests

--- src/main/java/com/javadiscord/jdi/internal/api/impl/user/GetUserRequest.java
+++ src/main/java/com/javadiscord/jdi/internal/api/impl/user/GetUserRequest.java
@@ -3,11 +3,11 @@ package com.javadiscord.jdi.internal.api.impl.user;
 import com.javadiscord.jdi.internal.api.DiscordRequest;
 import com.javadiscord.jdi.internal.api.DiscordRequestBuilder;
 
-public final record GetUserRequest(String userId) implements DiscordRequest {
+public final record GetUserRequest(long userId) implements DiscordRequest {
     @Override
     public DiscordRequestBuilder create() {
         return new DiscordRequestBuilder()
-            .path("/users/" + userId())
+            .path("/users/" + userId)
             .get();
     }
 }
--- src/main/java/com/javadiscord/jdi/internal/api/impl/user/ModifyCurrentUserRequest.java
+++ src/main/java/com/javadiscord/jdi/internal/api/impl/user/ModifyCurrentUserRequest.java
@@ -1,26 +1,27 @@
 package com.javadiscord.jdi.internal.api.impl.user;
 
 import java.util.Base64;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 import com.javadiscord.jdi.internal.api.DiscordRequest;
 import com.javadiscord.jdi.internal.api.DiscordRequestBuilder;
 
-public final record ModifyCurrentUserRequest(String username, byte[] avatarImage, String type)
+public final record ModifyCurrentUserRequest(String username, Optional<byte[]> avatarImage, String type)
     implements DiscordRequest {
     @Override
     public DiscordRequestBuilder create() {
-        String encodedAvatar = "data:image/%s;base64,%s"
-            .formatted(type, Base64.getEncoder().encodeToString(avatarImage));
+        Map<String, Object> payload = new HashMap<>();
+        payload.put("username", username);
+        avatarImage.ifPresent(
+            val -> payload
+                .put("avatar", "data:image/%s;base64,%s".formatted(type, Base64.getEncoder().encodeToString(val)))
+        );
 
         return new DiscordRequestBuilder()
             .path("/users/@me")
             .patch()
-            .body(
-                Map.of(
-                    "username", username,
-                    "avatar", encodedAvatar
-                )
-            );
+            .body(payload);
     }
 }
