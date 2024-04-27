406ea0ffa36c8d7f0bd4b7e91dd55f78cd50e11c
Suraj Kumar <sk96.uk@gmail.com>
2024-04-27 15:11:36 +0100

    Re-added integration tests

---
+++ api/src/test/integration/com/javadiscord/jdi/internal/api/application/EditCurrentApplicationRequestTest.java
@@ -0,0 +1,49 @@
+package com.javadiscord.jdi.internal.api.application;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import com.javadiscord.jdi.internal.models.application.ApplicationInstallParams;
+import helpers.RestTestHelper;
+import com.javadiscord.jdi.internal.api.DiscordRequest;
+import com.javadiscord.jdi.internal.api.DiscordResponseFuture;
+
+import org.junit.jupiter.api.Test;
+
+class EditCurrentApplicationRequestTest extends RestTestHelper {
+
+    @Test
+    void test() throws InterruptedException {
+        DiscordRequest request = new EditCurrentApplicationRequest(
+                "",
+                "",
+                "",
+                new ApplicationInstallParams(List.of(), ""),
+                Map.of(),
+                0,
+                "",
+                "",
+                "",
+                List.of()
+        );
+
+        DiscordResponseFuture response = getDiscordRequestDispatcher().queue(request);
+
+        CountDownLatch latch = new CountDownLatch(1);
+
+        response.onSuccess(
+                (r) -> {
+                    assertEquals(r.status(), 200);
+                    latch.countDown();
+                }
+        );
+
+        response.onError(err -> fail());
+
+        assertTrue(latch.await(5, TimeUnit.SECONDS));
+    }
+}
