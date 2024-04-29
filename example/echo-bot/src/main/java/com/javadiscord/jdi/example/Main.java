0d016e63427592fa3f144883578cf45e2f1da8b8
Suraj Kumar <sk96.uk@gmail.com>
2024-04-29 16:24:08 +0100

    Pre-Commit

--- example/echo-bot/src/main/java/com/javadiscord/jdi/example/Main.java
+++ example/echo-bot/src/main/java/com/javadiscord/jdi/example/Main.java
@@ -4,8 +4,7 @@ import com.javadiscord.jdi.core.Discord;
 
 public class Main {
     public static void main(String[] args) {
-        Discord discord =
-                new Discord("");
+        Discord discord = new Discord("");
         discord.registerListener(new ExampleNoAnnotate());
         discord.start();
     }
