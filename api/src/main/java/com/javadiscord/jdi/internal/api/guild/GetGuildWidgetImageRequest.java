1ae167a537e0c5037728b9e452045458e696dde3
Ashi <ashi_habibti@hotmail.com>
2024-05-05 19:13:52 +0100

    Implemented TODO comment and created WidgetStyle

--- api/src/main/java/com/javadiscord/jdi/internal/api/guild/GetGuildWidgetImageRequest.java
+++ api/src/main/java/com/javadiscord/jdi/internal/api/guild/GetGuildWidgetImageRequest.java
@@ -1,20 +1,20 @@
 package com.javadiscord.jdi.internal.api.guild;
 
+import com.javadiscord.jdi.core.models.guild.WidgetStyle;
 import com.javadiscord.jdi.internal.api.DiscordRequest;
 import com.javadiscord.jdi.internal.api.DiscordRequestBuilder;
 
 import java.util.Optional;
 
-public record GetGuildWidgetImageRequest(long guildId, Optional<String> style // TODO:
-        // https://discord.com/developers/docs/resources/guild#get-guild-widget-image-widget-style-options
-        ) implements DiscordRequest {
+public record GetGuildWidgetImageRequest(long guildId, Optional<WidgetStyle> style)
+        implements DiscordRequest {
 
     @Override
     public DiscordRequestBuilder create() {
         DiscordRequestBuilder discordRequestBuilder =
                 new DiscordRequestBuilder().get().path("/guilds/%s/widget.png".formatted(guildId));
 
-        style.ifPresent(val -> discordRequestBuilder.queryParam("style", val));
+        style.ifPresent(val -> discordRequestBuilder.queryParam("style", val.name().toLowerCase()));
 
         return discordRequestBuilder;
     }
---
+++ models/src/main/java/com/javadiscord/jdi/core/models/guild/WidgetStyle.java
@@ -0,0 +1,9 @@
+package com.javadiscord.jdi.core.models.guild;
+
+public enum WidgetStyle {
+    SHIELD,
+    BANNER1,
+    BANNER2,
+    BANNER3,
+    BANNER4,
+}
