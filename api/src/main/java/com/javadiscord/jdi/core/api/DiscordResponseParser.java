9b0655900adb71090dc1dca1cf0ff63c075565f0
NightTripperId <ndering@wgu.edu>
2024-05-23 07:41:08 -0700

    enhancement: adds caching to DiscordResponseParser

--- api/src/main/java/com/javadiscord/jdi/core/api/DiscordResponseParser.java
+++ api/src/main/java/com/javadiscord/jdi/core/api/DiscordResponseParser.java
@@ -6,16 +6,20 @@ import com.javadiscord.jdi.internal.api.DiscordRequest;
 import com.javadiscord.jdi.internal.api.DiscordRequestDispatcher;
 import com.javadiscord.jdi.internal.api.DiscordResponse;
 import com.javadiscord.jdi.internal.api.DiscordResponseFuture;
+import com.javadiscord.jdi.internal.cache.Cache;
 
+import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
 
 public class DiscordResponseParser {
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
     private final DiscordRequestDispatcher dispatcher;
+    private final Cache cache;
 
-    public DiscordResponseParser(DiscordRequestDispatcher dispatcher) {
+    public DiscordResponseParser(DiscordRequestDispatcher dispatcher, Cache cache) {
         this.dispatcher = dispatcher;
+        this.cache = cache;
     }
 
     public <T> AsyncResponse<List<T>> callAndParseList(Class<T> clazz, DiscordRequest request) {
@@ -27,26 +31,10 @@ public class DiscordResponseParser {
                         try {
                             List<T> resultList = parseResponseFromList(clazz, response.body());
                             asyncResponse.setResult(resultList);
-                        } catch (Exception e) {
-                            asyncResponse.setException(e);
-                        }
-                    } else {
-                        asyncResponse.setException(errorResponseException(response));
-                    }
-                });
-        future.onError(asyncResponse::setException);
-        return asyncResponse;
-    }
-
-    public <T> AsyncResponse<List<T>> callAndParseMap(String key, DiscordRequest request) {
-        AsyncResponse<List<T>> asyncResponse = new AsyncResponse<>();
-        DiscordResponseFuture future = dispatcher.queue(request);
-        future.onSuccess(
-                response -> {
-                    if (isSuccessfulResponse(response)) {
-                        try {
-                            List<T> resultList = parseResponseFromMap(key, response.body());
-                            asyncResponse.setResult(resultList);
+                            for (T result : resultList) {
+                                cacheResult(result);
+                            }
+                        } catch (NoSuchFieldException e) { // ignore
                         } catch (Exception e) {
                             asyncResponse.setException(e);
                         }
@@ -63,6 +51,30 @@ public class DiscordResponseParser {
         return OBJECT_MAPPER.readValue(
                 response,
                 OBJECT_MAPPER.getTypeFactory().constructCollectionType(List.class, elementType));
+    }
+
+    public <T> AsyncResponse<List<T>> callAndParseMap(String key, DiscordRequest request) {
+        AsyncResponse<List<T>> asyncResponse = new AsyncResponse<>();
+        DiscordResponseFuture future = dispatcher.queue(request);
+        future.onSuccess(
+                response -> {
+                    if (isSuccessfulResponse(response)) {
+                        try {
+                            List<T> resultList = parseResponseFromMap(key, response.body());
+                            asyncResponse.setResult(resultList);
+                            for(T result : resultList) {
+                                cacheResult(result);
+                            }
+                        } catch (NoSuchFieldException e) { // ignore
+                        } catch (Exception e){
+                            asyncResponse.setException(e);
+                        }
+                    } else {
+                        asyncResponse.setException(errorResponseException(response));
+                    }
+                });
+        future.onError(asyncResponse::setException);
+        return asyncResponse;
     }
 
     private <T> List<T> parseResponseFromMap(String key, String response)
@@ -90,12 +102,32 @@ public class DiscordResponseParser {
             try {
                 T result = OBJECT_MAPPER.readValue(response.body(), type);
                 asyncResponse.setResult(result);
-            } catch (JsonProcessingException e) {
+                cacheResult(result);
+            } catch(NoSuchFieldException e) { // ignore
+            } catch (JsonProcessingException | IllegalAccessException e) {
                 asyncResponse.setException(e);
             }
         } else {
             asyncResponse.setException(errorResponseException(response));
         }
+    }
+
+    private <T> void cacheResult(T result) throws IllegalAccessException, NoSuchFieldException {
+        Field guildIdField = result.getClass().getDeclaredField("guildId");
+        Field idField = result.getClass().getDeclaredField("id");
+
+        long guildId = getLongFromField(guildIdField);
+        long id = getLongFromField(idField);
+
+        cache.getCacheForGuild(guildId).add(id, result);
+    }
+
+    private long getLongFromField(Field field) throws IllegalAccessException {
+        field.setAccessible(true);
+        if (field.getType() == String.class) {
+            return Long.parseLong((String) field.get(field.getName()));
+        }
+        return (long) field.get(field.getName());
     }
 
     private boolean isSuccessfulResponse(DiscordResponse response) {
