0e43d5f82c35840fe40b3dc6f51796331cad841c
Suraj Kumar <sk96.uk@gmail.com>
2024-05-23 21:20:49 +0100

    Fix example to actually be an echo bot

--- example/echo-bot/src/main/java/com/javadiscord/jdi/example/EchoHandler.java
+++ example/echo-bot/src/main/java/com/javadiscord/jdi/example/EchoHandler.java
@@ -3,6 +3,7 @@ package com.javadiscord.jdi.example;
 import com.javadiscord.jdi.core.Guild;
 import com.javadiscord.jdi.core.annotations.EventListener;
 import com.javadiscord.jdi.core.annotations.MessageCreate;
+import com.javadiscord.jdi.core.api.builders.CreateMessageBuilder;
 import com.javadiscord.jdi.core.models.message.Message;
 
 @EventListener
@@ -10,6 +11,10 @@ public class EchoHandler {
 
     @MessageCreate
     public void echoMessages(Message message, Guild guild) {
-        System.out.println("EchoHandler");
+        if (!message.author().bot()) {
+            guild.channel().createMessage(
+                new CreateMessageBuilder(message.channelId()).content(message.content())
+            );
+        }
     }
 }
