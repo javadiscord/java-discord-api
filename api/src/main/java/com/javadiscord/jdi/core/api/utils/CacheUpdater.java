f432fbfac913f77c09808171e213204d4ca8a4a4
NightTripperId <ndering@wgu.edu>
2024-05-25 13:09:32 -0700

    chore: adds tracing logs

--- api/src/main/java/com/javadiscord/jdi/core/api/utils/CacheUpdater.java
+++ api/src/main/java/com/javadiscord/jdi/core/api/utils/CacheUpdater.java
@@ -5,9 +5,15 @@ import java.util.List;
 
 import com.javadiscord.jdi.internal.cache.Cache;
 
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class CacheUpdater {
 
     private final Cache cache;
+
+    private static final Logger LOGGER = LogManager.getLogger(CacheUpdater.class);
 
     public CacheUpdater(Cache cache) {
         this.cache = cache;
@@ -24,11 +30,20 @@ public class CacheUpdater {
             long guildId = getLongFromField(guildIdField, result);
             long id = getLongFromField(idField, result);
 
-            if (cache.getCacheForGuild(guildId) != null) {
+            if (cache.getCacheForGuild(guildId) == null) {
+                LOGGER.log(
+                    Level.TRACE, "Failed to cache result of type {} with guildId of {}",
+                    result.getClass().getName(), guildId
+                );
+            } else {
                 cache.getCacheForGuild(guildId).add(id, result);
             }
-        } catch (IllegalAccessException | NoSuchFieldException e) {
-            // NoSuchFieldException: don't cache result
+
+        } catch (IllegalAccessException | NoSuchFieldException | NumberFormatException e) {
+            LOGGER.log(
+                Level.TRACE, "Failed to cache result of type {}, cause: {}",
+                result.getClass().getName(), e.getMessage()
+            );
         }
     }
 
@@ -36,10 +51,21 @@ public class CacheUpdater {
         resultList.forEach(this::updateCache);
     }
 
-    private <T> long getLongFromField(Field field, T result) throws IllegalAccessException {
+    private <T> long getLongFromField(
+        Field field,
+        T result
+    ) throws IllegalAccessException, NumberFormatException {
         field.setAccessible(true);
         if (field.getType() == String.class) {
-            return Long.parseLong((String) field.get(result));
+            try {
+                return Long.parseLong((String) field.get(result));
+            } catch (NumberFormatException e) {
+                throw new NumberFormatException(
+                    String.format(
+                        "Failed to parse %s=%s as a long", field.getName(), field.get(result)
+                    )
+                );
+            }
         }
         return (long) field.get(result);
     }
