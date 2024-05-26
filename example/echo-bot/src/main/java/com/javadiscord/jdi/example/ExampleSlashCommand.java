c95f8997d97a6b7c972228d58203d9b1698e87d2
Suraj Kumar <sk96.uk@gmail.com>
2024-05-26 17:08:50 +0100

    Added ExampleSlashCommand

---
+++ example/echo-bot/src/main/java/com/javadiscord/jdi/example/ExampleSlashCommand.java
@@ -0,0 +1,24 @@
+package com.javadiscord.jdi.example;
+
+import com.javadiscord.jdi.core.CommandOptionType;
+import com.javadiscord.jdi.core.annotations.CommandOption;
+import com.javadiscord.jdi.core.annotations.EventListener;
+import com.javadiscord.jdi.core.annotations.SlashCommand;
+
+@EventListener
+public class ExampleSlashCommand {
+
+    @SlashCommand(
+            name = "quiz",
+            description = "A fun Java quiz",
+            options = {
+                @CommandOption(name = "q1", description = "What is an Integer?", type = CommandOptionType.STRING),
+                @CommandOption(name = "q2", description = "What package is List in?", type = CommandOptionType.STRING),
+                @CommandOption(name = "q3", description = "What does JVM stand for?", type = CommandOptionType.STRING),
+                @CommandOption(name = "q4", description = "Is a String a primitive?", type = CommandOptionType.STRING),
+            }
+    )
+    public void handle() {
+        //TODO: Logic to handle the slash command
+    }
+}
