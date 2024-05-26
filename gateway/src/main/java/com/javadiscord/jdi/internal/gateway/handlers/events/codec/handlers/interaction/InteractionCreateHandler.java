8e43b2afcde8b342c5ebbb4542ca4a16bd8f3263
Suraj Kumar <sk96.uk@gmail.com>
2024-05-26 17:04:14 +0100

    chore: Remove add to cache for an interaction

--- gateway/src/main/java/com/javadiscord/jdi/internal/gateway/handlers/events/codec/handlers/interaction/InteractionCreateHandler.java
+++ gateway/src/main/java/com/javadiscord/jdi/internal/gateway/handlers/events/codec/handlers/interaction/InteractionCreateHandler.java
@@ -6,8 +6,7 @@ import com.javadiscord.jdi.internal.gateway.ConnectionMediator;
 import com.javadiscord.jdi.internal.gateway.handlers.events.codec.EventHandler;
 
 public class InteractionCreateHandler implements EventHandler<Interaction> {
+
     @Override
-    public void handle(Interaction event, ConnectionMediator connectionMediator, Cache cache) {
-        cache.getCacheForGuild(event.guildId()).add(event.id(), event);
-    }
+    public void handle(Interaction event, ConnectionMediator connectionMediator, Cache cache) {}
 }
