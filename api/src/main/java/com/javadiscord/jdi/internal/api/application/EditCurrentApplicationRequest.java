f07c0a31fbbb3e8e28e4aeae4a08beb3717786b6
Connor Schweighoefer <squidxtv@gmail.com>
2024-07-24 00:29:01 +0200

    Add integration tests for application requests

--- api/src/main/java/com/javadiscord/jdi/internal/api/application/EditCurrentApplicationRequest.java
+++ api/src/main/java/com/javadiscord/jdi/internal/api/application/EditCurrentApplicationRequest.java
@@ -1,43 +1,46 @@
 package com.javadiscord.jdi.internal.api.application;
 
-import java.util.List;
-import java.util.Map;
-
 import com.javadiscord.jdi.core.models.application.ApplicationInstallParams;
 import com.javadiscord.jdi.internal.api.DiscordRequest;
 import com.javadiscord.jdi.internal.api.DiscordRequestBuilder;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
 public record EditCurrentApplicationRequest(
-    String customInstallUrl,
-    String description,
-    String roleConnectionsVerificationUrl,
-    ApplicationInstallParams installParams,
-    Map<String, Object> integrationTypesConfig,
-    int flags,
-    String icon,
-    String coverImage,
-    String interactionsEndpointUrl,
-    List<String> tags
+        Optional<String> customInstallUrl,
+        Optional<String> description,
+        Optional<String> roleConnectionsVerificationUrl,
+        Optional<ApplicationInstallParams> installParams,
+        Optional<Map<String, Object>> integrationTypesConfig,
+        Optional<Integer> flags,
+        Optional<String> icon,
+        Optional<String> coverImage,
+        Optional<String> interactionsEndpointUrl,
+        Optional<List<String>> tags
 ) implements DiscordRequest {
 
     @Override
     public DiscordRequestBuilder create() {
+        Map<String, Object> body = new HashMap<>();
+
+        customInstallUrl.ifPresent(val -> body.put("custom_install_url", val));
+        description.ifPresent(val -> body.put("description", val));
+        roleConnectionsVerificationUrl.ifPresent(val -> body.put("role_connections_verification_url", val));
+        installParams.ifPresent(val -> body.put("install_params", val));
+        integrationTypesConfig.ifPresent(val -> body.put("integration_types_config", val));
+        flags.ifPresent(val -> body.put("flags", val));
+        icon.ifPresent(val -> body.put("icon", val));
+        coverImage.ifPresent(val -> body.put("cover_image", val));
+        interactionsEndpointUrl.ifPresent(val -> body.put("interactions_endpoint_url", val));
+        tags.ifPresent(val -> body.put("tags", val));
+
         return new DiscordRequestBuilder()
-            .patch()
-            .path("/applications/@me")
-            .body(
-                Map.of(
-                    "custom_install_url", customInstallUrl,
-                    "description", description,
-                    "role_connections_verification_url", roleConnectionsVerificationUrl,
-                    "install_params", installParams,
-                    "integration_types_config", integrationTypesConfig,
-                    "flags", flags,
-                    "icon", icon,
-                    "cover_image", coverImage,
-                    "interactions_endpoint_url", interactionsEndpointUrl,
-                    "tags", tags
-                )
-            );
+                .patch()
+                .path("/applications/@me")
+                .body(body);
     }
+
 }
